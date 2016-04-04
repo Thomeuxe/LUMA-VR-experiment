ae1003b0394b32143f519a1e36f548710188f106
Rémy <remy@sleede.com>
2016-04-04 16:25:09 +0200

    merge

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,8 +3,9 @@ var camera = require('./Utils/Camera').create();
 var renderer = require('./Utils/Renderer').create();
 
 var terrain = require('./Terrain').create(scene, camera, renderer);
-var sounds = require('./Sounds');
+var sounds = require('./Sounds').create();
 var controls = require('./Controls').create(camera);
+var terrain = require('./Terrain');
 var UI = require('./UI');
 
 var render = function() {
