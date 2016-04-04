ddec0c23a7fa675a8f6de9717580aa5d97aabbe8
Thomas Lecoeur <me@thomaslecoeur.com>
2016-04-04 20:11:40 +0200

    Add pixel ratio renderer size support

--- src/assets/js/Utils/Renderer/index.js
+++ src/assets/js/Utils/Renderer/index.js
@@ -1,7 +1,10 @@
 var Renderer = {
     create: function() {
         this.renderer = new THREE.WebGLRenderer();
+
         this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+        
         document.body.appendChild(this.renderer.domElement);
 
         return this.renderer;
