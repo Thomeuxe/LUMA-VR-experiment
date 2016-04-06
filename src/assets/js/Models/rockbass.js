da271e73ffeffa74077e4e3f659d4b89f6340c76
Rémy <remy@sleede.com>
2016-04-06 12:21:15 +0200

    add rockbass fish

---
+++ src/assets/js/Models/rockbass.js
@@ -0,0 +1,103 @@
+var Rockbass = function (infos) {
+  infos = infos || {};
+
+  this.name = infos.name;
+
+  if (!Rockbass._base) {
+    // Start object loading
+    if (!Rockbass._loadingBase) {
+      Rockbass.loader.load('assets/js/Models/skinned/rockbass.json', Rockbass._setBase);
+      Rockbass._loadingBase = true;
+    }
+    // Add current object to wait list
+    Rockbass._needsInit.push(this);
+  } else {
+    this.init();
+  }
+
+  return this;
+};
+
+Rockbass._base = null;
+Rockbass._loadingBase = false;
+Rockbass._needsInit = [];
+
+Rockbass._setBase = function(geometry, materials) {
+  materials.forEach( function (material) {
+    material.skinning = true;
+  } );
+
+  Rockbass._base = new THREE.SkinnedMesh( geometry, new THREE.MultiMaterial( materials ) );
+  Rockbass._loadingBase = false;
+  // Instanciate wait list objects
+  for (var i = 0; i < Rockbass._needsInit.length; i++) {
+    Rockbass._needsInit[i].init();
+  }
+};
+
+Rockbass.loader = new THREE.JSONLoader();
+Rockbass.instances = [];
+
+Rockbass.setScene = function (scene) {
+  Rockbass.scene = scene;
+};
+
+Rockbass.setListener = function (listener) {
+  Rockbass.listener = listener;
+};
+
+Rockbass.update = function (delta) {
+  Rockbass.instances.forEach(function (instance) {
+    if (instance.mixer) {
+      instance.mixer.update(delta);
+    }
+  });
+};
+
+Rockbass.prototype.init = function () {
+  this.mesh = Rockbass._base.clone();
+
+  this.mesh.position.x = Math.random() * 2000;
+  this.mesh.position.y = Math.random() * 2000;
+  this.mesh.position.z = Math.random() * 2000;
+
+  this.mesh.name = this.name;
+
+  Rockbass.scene.add(this.mesh);
+
+  var box = new THREE.Box3().setFromObject(this.mesh);
+  var geometryCollider = new THREE.SphereGeometry(box.getBoundingSphere().radius, 6, 6);
+  this.meshCollider = new THREE.Mesh(geometryCollider, new THREE.MeshBasicMaterial({wireframe: true}));
+  this.mesh.add(this.meshCollider);
+
+  this.mesh.castShadow = true;
+  this.mesh.receiveShadow = true;
+
+  this.meshCollider.targetable = true;
+
+  this.initAnimation(this.mesh);
+  this.initAudio(Rockbass.listener);
+
+  Rockbass.instances.push(this);
+};
+
+Rockbass.prototype.initAnimation = function (object) {
+  this.mixer = new THREE.AnimationMixer(this.mesh);
+
+  console.log(object);
+
+  this.action = this.mixer.clipAction( object.geometry.animations[ 0 ] );
+  this.action.play();
+};
+
+Rockbass.prototype.initAudio = function (listener) {
+  this.audio = new THREE.PositionalAudio( listener );
+  this.audio.load( 'assets/sounds/fish_noise.mp3' );
+  this.audio.setRefDistance( 4 );
+  this.audio.setLoop(true);
+  this.audio.autoplay = true;
+  this.audio.setVolume(3);
+  this.mesh.add( this.audio );
+};
+
+module.exports = Rockbass;
