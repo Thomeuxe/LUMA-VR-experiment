51759f84b293761283c08771835b8ded0a15a53c
Quentin Neyraud <quentin.neyraud@gmail.com>
2016-04-06 13:58:35 +0200

    move main function to UI

--- src/assets/js/UI/index.js
+++ src/assets/js/UI/index.js
@@ -1,8 +1,11 @@
+var dbg = require('debug')('luma:UI');
+
 var UI = {
     infoPanel: null,
     font: null,
 
     createTarget: function(camera) {
+        dbg('create target');
         var target = new THREE.Mesh(new THREE.CircleGeometry(0.01), new THREE.MeshBasicMaterial({color: 0x5555ff}));
         camera.add(target);
         target.position.set(0,0,-1.2);
@@ -23,6 +26,7 @@ var UI = {
         if(!parent) return;
 
         if (!this.infoPanel) {
+            dbg('open info panel');
             this.infoPanel = new THREE.Object3D();
             this.infoName = new THREE.Mesh(
               new THREE.TextGeometry(parent.parent.name, {font: this.font, size: 13, height: 1}),
@@ -44,8 +48,45 @@ var UI = {
 
     closeInfoPanel: function() {
         if (!this.infoPanel) return;
+        dbg('close info panel');
 
         this.infoPanel.visible = false;
+    },
+
+    toggleFullScreen: function(wrapper, supports) {
+        dbg('toggle full screen');
+        if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+            if (wrapper.requestFullscreen) {
+                wrapper.requestFullscreen();
+            } else if (wrapper.mozRequestFullScreen) {
+                wrapper.mozRequestFullScreen();
+            } else if (wrapper.webkitRequestFullscreen) {
+                wrapper.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else {
+            if (document.cancelFullScreen) {
+                document.cancelFullScreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            }
+        }
+
+        if(supports.isMobile()) {
+            screen.orientation.lock("landscape-primary");
+        }
+    },
+
+    toggleAudio: function(listener) {
+        dbg('toggle audio');
+        listener.toggle();
+    },
+
+    onWindowResize: function(camera, renderer) {
+        dbg('window resize');
+        camera.handleResize();
+        renderer.handleResize();
     }
 };
 
