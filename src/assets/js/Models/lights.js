e4ac18ee07958b2b2bba48f8e97227d10d812cae
Rémy <remy@sleede.com>
2016-04-08 16:27:22 +0200

    improve lights code

--- src/assets/js/Models/lights.js
+++ src/assets/js/Models/lights.js
@@ -1,12 +1,13 @@
 var Lights = {
-    addAsChild: function(object, scene) {
-        var light = new THREE.PointLight( 0xffffff, 2, 300 );
-        light.position.set(0,2,1);
-        object.add(light);
-        var light = new THREE.PointLight( 0xffffff, 2, 300 );
-        light.position.set(0,2,-1);
-        object.add(light);
-    }
+  addAsChild: function (object, scene) {
+    var light;
+    var lightPositions = [new THREE.Vector3(0, 2, 1), new THREE.Vector3(0, 2, -1)];
+    lightPositions.forEach(function (pos) {
+      light = new THREE.PointLight(0xffffff, 2, 300);
+      light.position.set(pos.x, pos.y, pos.z);
+      object.add(light);
+    });
+  }
 };
 
 module.exports = Lights;
