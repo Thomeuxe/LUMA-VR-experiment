9d0741fb62869dc71ebf2f4d264ed46cd6ce74c4
Rémy <remy@sleede.com>
2016-04-08 16:10:04 +0200

    reduce light

--- src/assets/js/Models/lights.js
+++ src/assets/js/Models/lights.js
@@ -1,9 +1,9 @@
 var Lights = {
     addAsChild: function(object, scene) {
-        var light = new THREE.PointLight( 0xffffff, 6, 800 );
+        var light = new THREE.PointLight( 0xffffff, 2, 300 );
         light.position.set(0,2,1);
         object.add(light);
-        var light = new THREE.PointLight( 0xffffff, 6, 800 );
+        var light = new THREE.PointLight( 0xffffff, 2, 300 );
         light.position.set(0,2,-1);
         object.add(light);
     }
