8bff37e743d6b343d99bb04db25a8ff273234bed
Kubenexion <kubenexion@gmail.com>
2016-04-11 20:48:22 +0200

    Update starting position

--- src/assets/js/Utils/Camera/index.js
+++ src/assets/js/Utils/Camera/index.js
@@ -1,7 +1,7 @@
 var Camera = {
     create: function() {
         this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        this.camera.position.setY(210);
+        this.camera.position.setY(130);
         this.camera.position.setZ(10);
         this.camera.acceleration = 0;
 
